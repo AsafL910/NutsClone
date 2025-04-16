737f558679ddeb1c8f9ae5ceebf98f3b38ee43e6
Asaf <asaflumer@gmail.com>
2025-04-16 16:05:44 +0300

    Changed branch prefab to include lasers instead of trees

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -4,6 +4,8 @@ public class Camera : MonoBehaviour
 {
   public Transform target;
 
+public float offset = 0f;
+  public float angle = 0f;
   public float distance = 3;
 
 private void Start() {
@@ -11,10 +13,10 @@ private void Start() {
     private void LateUpdate()
     {
             // Update the camera's position based on the player's position and offset
-            transform.position = distance*target.position;
+            transform.position = distance*target.position + Vector3.down * offset;
 
             // Make the camera look at the player's position
-            transform.LookAt(target);
+            transform.LookAt(target.position + Vector3.up * distance * Mathf.Tan(Mathf.Deg2Rad * angle));
         
     }
 }
