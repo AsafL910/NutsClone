216e4e36db701dee1146aecff40110643b5d78d2
Asaf <asaflumer@gmail.com>
2025-04-19 23:13:40 +0300

    Fixed spinning obstacles to work properly

--- Assets/Scripts/Spawners/SpinningBranchSpawner.cs
+++ Assets/Scripts/Spawners/SpinningBranchSpawner.cs
@@ -3,9 +3,14 @@ using UnityEngine;
 public class SpinningBranchSpawner : Spawner
 {
     public GameObject obstaclePrefab;
-
+    protected override void Spawn()
+    {
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+        Transform spawnPoint = transform;
+        PerformSpawn(spawnPoint);
+    }
     protected override void PerformSpawn(Transform spawnPoint)
     {
-        Instantiate(obstaclePrefab, spawnPoint.position, Quaternion.Euler(0, 90, 90));
+        Instantiate(obstaclePrefab, spawnPoint.position, transform.rotation * Quaternion.Euler(0,90,90));
     }
 }
